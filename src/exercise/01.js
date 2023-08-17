211ba2fc540aac766ef9f263b91a51bbb77da3da
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-17 12:03:18 +0330

    Finish the third extra credit of the first exercise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,15 +2,20 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
+import {act} from 'react-dom/test-utils'
 
-const countReducer = (state, action) => ({...state, ...action})
+const countReducer = (state, action) => ({
+  ...state,
+  ...(typeof action === 'function' ? action(state) : action),
+})
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
